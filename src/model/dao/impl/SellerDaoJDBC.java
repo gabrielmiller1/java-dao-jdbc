0ec7f71609477cbddda37b837e9348b3eb816d3b
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-06-22 13:04:15 -0300

    Creating SellerDaoJDB and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+
+public class SellerDaoJDBC implements SellerDao {
+    
+    @Override
+    public void insert(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Seller findById(Integer Id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+    
+}
