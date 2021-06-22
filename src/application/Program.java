87fd73fec12324d551cc17b61d86b08cd85e887a
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-06-22 18:29:32 -0300

    findAll implamentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,11 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
-
+        
+        System.out.println("\n=== TEST 3: seller findAll =====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
