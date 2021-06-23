7932dbda2b4beaed45903bffdae6f9fb33fe010d
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-06-23 00:33:40 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,9 @@ import model.entities.Seller;
 public class Program {
 
     public static void main(String[] args) {
-
+        
+        Scanner sc = new Scanner(System.in);
+        
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById =====");
@@ -41,5 +44,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+        
+        System.out.println("\n=== TEST 5: seller update =====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+        
+        sc.close();
     }
 }
